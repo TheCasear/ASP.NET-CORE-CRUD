aa6ff98792e569047b4467e2b58222b0ad11d552
nickbodaveli <nickbodaveli@gmail.com>
2022-06-11 06:01:03 -0700

    MVC CRUD

---
+++ CRUD/Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
